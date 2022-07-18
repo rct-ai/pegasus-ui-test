a703b8e259e6e8475ed529603735242c72b2a59b
della0506 <106648768+della0506@users.noreply.github.com>
2022-07-18 14:26:49 +0800

    Add files via upload

---
+++ common_func.py
@@ -0,0 +1,16 @@
+
+
+class commonFunc():
+
+    def baseUrl(self):
+        """测试环境"""
+        baseUrl = 'https://pegasus-admin-test.rct.ai'
+        return baseUrl
+
+'''
+    def testUser(self):
+        """测试账户"""
+        username = 'pegasus@rct.ai'
+        password = 'pegasus123456'
+        return username,password
+'''
---
+++ test_asso.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import seldom
+from seldom import Seldom
+
+from func.common_func import commonFunc
+from pages.project_page import ProjectPage
+
+class testSSO(seldom.TestCase):
+    def start(self):
+        """初始化"""
+        page = ProjectPage(Seldom.driver)
+        page.open(commonFunc.baseUrl(self) + ProjectPage.project_url)
+        self.max_window()
+
+    def down(self):
+        """关闭浏览器"""
+        self.quit()
+        self.close()
+
+    def test01(self):
+        """sso登录"""
+        self.add_cookie({'name' : 'sso-t', 'value' : 'L7W6E4IA4LS6U3TYCIW3YHGUMJVB6PAUVVIXTZYSPPUPFXVAALE4XGSYTAFEZ2AD6WB6XZ37YPOTVQYRABV2BYYXA4T4ERJXDGLOGZQ','Domain' : '.rct.ai','Path' : '/'})
+        self.refresh()
+        self.assertText('管理后台')
+
+
+
